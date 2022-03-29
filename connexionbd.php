b2181eaba7830d4a9ffa32d0bef6dcecfc1291c3
Boris Arnaud TABWE ESSOME <34147941+spiritua@users.noreply.github.com>
2022-03-29 11:53:17 +0200

    mise à jour connexion à la bd sqlite

--- connexionbd.php
+++ connexionbd.php
@@ -1,11 +1,24 @@
 <?php
 // Connexion à la base de données
-try
+
+try{
+    $fichierDb='indev.db';
+    $bdd=new PDO('sqlite:'.$fichierDb);
+}
+catch(Exception $e)
+{
+    die ('Erreur: '.$e->getMessage());
+}
+
+
+
+
+/*try
 {
 $bdd = new PDO('mysql:host=localhost;dbname=indev', 'root', '');
 }
 catch(Exception $e)
 {
 die('Erreur : '.$e->getMessage());
-}
+}*/
 ?>
