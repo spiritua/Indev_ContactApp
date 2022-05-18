7327a19ce8b2029d1d283f0329953566c5c0d18d
Boris Arnaud TABWE ESSOME <34147941+spiritua@users.noreply.github.com>
2022-05-18 12:54:20 +0200

    Updatetest

---
+++ tests/ModificationTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class ModificationTest extends TestCase
+{
+    public function testmodification(){
+        
+        require_once("connexionbd.php");
+        $req = $bdd->query("UPDATE contact SET nom = 'boss' ,
+        prenom = 'lion' WHERE nom = 'arnaur'");
+        $req->execute();
+
+        $req = $bdd->query('SELECT * from contact WHERE nom = "boss"');
+        $res = $req->fetchAll();
+        $this->assertEquals( 1, count($res)); 
+    }
+}
+?>
